2ad5f429de1df26d19fa1dbaaef89911637499a7
kingdevscode <p.djemne@gmail.com>
2021-01-05 19:22:31 +0100

    festival informations

--- src/app/main/main.page.ts
+++ src/app/main/main.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 
 @Component({
   selector: 'app-main',
@@ -6,10 +7,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./main.page.scss'],
 })
 export class MainPage implements OnInit {
-
-  constructor() { }
+  
+  data : any;
+  constructor(
+    public http: HttpClient
+  ){
+    this.getArticle()
+  }
 
   ngOnInit() {
   }
 
+  getArticle(){
+    let url = 'http://newsapi.org/v2/everything?q=cultural%20festival&from=2020-12-05&sortBy=publishedAt&apiKey=1f226b8f2beb45ceb00223a8e2c68ce3';
+    this.http.get(url).subscribe(data => {
+      this.data = data;
+    })
+  }
 }
